b207492c5699962d6ac5d3d989d5855486d28495
Kevin Schaefer <keschae@ilstu.edu>
2018-02-26 14:14:38 -0600

    Changed config.php

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = '/ci/';
+$config['base_url'] = '/assignment4/';
 
 /*
 |--------------------------------------------------------------------------
